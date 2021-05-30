eb4d926f3331d50706cf8d481b2ca7adc1d763c9
MaelysSob <maelys.sobieraj@student.ehb.be>
2021-05-31 01:05:49 +0200

    changed card horizontal slider animation switches , gone from top nav to bottom navigation

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,10 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+
+
 class HomePage extends StatefulWidget {
   final VoidCallback onSignOut;
+
 
   const HomePage({Key key, @required this.onSignOut}) : super(key: key);
 
@@ -25,101 +31,354 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+
+
+
   bool value = false;
-  onUpdate(){
+  bool value2= false;
+  bool value3= false;
+  bool value4= false;
+
+  final dbRef = FirebaseDatabase.instance.reference();
+  onToggle(bool toggle){
     setState(() {
-      value=!value;// toggle between true and false
+      toggle=!toggle;
+      // toggle between true and false
     });
   }
-  @override
+  onUpdate() {
+    setState(() {
+      value =  !value;
+      value4=  !value4;// toggle between true and false
+    });
+  }
+  onUpdate2() {
+    setState(() {
+      value2 =  !value2;
+     // toggle between true and false
+    });
+  }
+  onUpdate3() {
+    setState(() {
+      value3 =  !value3;
+      // toggle between true and false
+    });
+  }
+  onUpdate4() {
+    setState(() {
+      value4 =  !value4;
+      // toggle between true and false
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(actions: <Widget>[
-        TextButton(
-          onPressed: signOut,
-          child: Text(
-            'Logout',
-            style: TextStyle(fontSize: 18, color: Colors.white),
-          ),
-        ),
-      ]),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Card(
-                  child: Padding(
-                    padding: const EdgeInsets.only(left:20,top:10,right: 20,bottom: 10),
-                    child: Text(
 
-                      "Temperature \n 20",// placeholder for data from sensor
-                      textAlign: TextAlign.center,
-                      style:
-                      TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-                Card(
-                  child: Padding(
-                    padding: const EdgeInsets.only(left:35,top:10,right: 35,bottom: 10),
-                    child: Text(
-                      "Humidity\n60",
-                      textAlign: TextAlign.center,
-                      style:
-                      TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                height: 500,
-                width: 350,
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  color: Colors.lightGreen.shade50,
-
-                  child: ButtonBar(
-
-
-                    alignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top: 350),
-
-
-                        child: ElevatedButton(onPressed: (){
-                          onUpdate();
-                          onWrite();// write data to firebase to turn on lamp
-                        }, child: value? Text(' Turn UV-Light ON'):Text('Turn UV-Light OFF'),
-                          style: ElevatedButton.styleFrom(
-                            primary: value? Colors.lightGreen.shade300:Colors.lightGreen.shade100
+      body: StreamBuilder(// to listen to data async
+        stream: dbRef.child("Data").onValue,// fires when data is updated
+        // ignore: missing_return
+        builder: (context, snapshot) {
+          if (snapshot.hasData && !snapshot.hasError &&
+              snapshot.data.snapshot.value != null) {
+            final temp = snapshot.data.snapshot.value["Temperature"].toString();
+            final humidity = snapshot.data.snapshot.value["Humidity"].toString();
+            return SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top:50),
+                    child: Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Card(
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 20, top: 10, right: 20, bottom: 10),
+                              child: Text(
+                                "Temperature \n $temp",
+                                // placeholder for data from sensor
+                                textAlign: TextAlign.center,
+                                style:
+                                TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold),
+                              ),
+                            ),
                           ),
-
-
-                        ),
-
+                          Card(
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 35, top: 10, right: 35, bottom: 10),
+                              child: Text(
+                                "Humidity\n  $humidity%",
+                                textAlign: TextAlign.center,
+                                style:
+                                TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-
-
-                    ],
+                    ),
                   ),
-                ),
-              )
-            ],
-          )
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 35.0),
+                    height: 430,
+                    child: ListView(
+                     scrollDirection: Axis.horizontal,
+                      children:<Widget> [
+                        Container(
+                          width:350,
+                          child: Card(
+
+
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            color: Colors.green.shade50,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: value 
+                                  ? AssetImage("images/hanging_lamp.png",
+                                  )
+                                    : AssetImage("images/UVAhanging_lamp.png"),
+
+                                    fit: BoxFit.fitHeight,
+                                  alignment: Alignment.topCenter
+                                ),
+                              ),
+                              child: ButtonBar(
+                                alignment: MainAxisAlignment.center,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 350.0),
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        onUpdate();
+                                        writeData("UVLightstate", value); // write data to firebase to turn on lamp
+                                      },
+                                      child: value
+                                          ? Text(' UV-Light ON')
+                                          : Text(' UV-Light OFF'),
+                                      style: ElevatedButton.styleFrom(
+                                          primary: value
+                                              ? Colors.lightGreen.shade100
+                                              : Colors.lightGreen.shade300),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                       SizedBox(width:20),
+                        Container(
+                          width:350,
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            color: Colors.green.shade50,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image:  value2
+                                  ? AssetImage("images/hanging_lamp.png",
+                                    )
+                                   : AssetImage("images/UVBhanging_lamp.png"),
+
+                                    fit: BoxFit.fitHeight,
+                                    alignment: Alignment.topCenter
+                                ),
+                              ),
+                              child: ButtonBar(
+                                alignment: MainAxisAlignment.center,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 350.0),
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        onUpdate2();
+                                        writeData("UVBLightstate", value2); // write data to firebase to turn on lamp
+                                      },
+                                      child: value2
+                                          ? Text('  UVB-Light ON')
+                                          : Text(' UVB-Light OFF'),
+                                      style: ElevatedButton.styleFrom(
+                                          primary: value2
+                                              ? Colors.lightGreen.shade100
+                                              : Colors.lightGreen.shade300),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width:20),
+                        Container(
+                          width:350,
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            color: Colors.green.shade50,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: value3
+                                  ? AssetImage("images/hanging_lamp.png",
+                                    )
+                                    : AssetImage("images/Temphanging_lamp.png"),
+
+
+                                    fit: BoxFit.fitHeight,
+                                    alignment: Alignment.topCenter
+                                ),
+                              ),
+                              child: ButtonBar(
+                                alignment: MainAxisAlignment.center,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 350.0),
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        onUpdate3();
+                                        writeData("TempLightstate", value3); // write data to firebase to turn on lamp
+                                      },
+                                      child: value3
+                                          ? Text('  Temp-Light ON')
+                                          : Text(' Temp-Light OFF'),
+                                      style: ElevatedButton.styleFrom(
+                                          primary: value3
+                                              ? Colors.lightGreen.shade100
+                                              : Colors.lightGreen.shade300),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width:20),
+                        Container(
+                          width:350,
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            color: Colors.green.shade50,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage("images/hanging_lamp.png",
+                                    ),
+
+                                    fit: BoxFit.scaleDown,
+                                    alignment: Alignment.topCenter
+                                ),
+                              ),
+                              child: ButtonBar(
+                                alignment: MainAxisAlignment.center,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 350.0),
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        onUpdate4();
+                                      writeData("switch4", value4); // write data to firebase to turn on lamp
+                                      },
+                                      child: value4
+                                          ? Text('  switch ON')
+                                          : Text(' switch OFF'),
+                                      style: ElevatedButton.styleFrom(
+                                          primary: value4
+                                              ? Colors.lightGreen.shade100
+                                              : Colors.lightGreen.shade300),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width:20),
+
+
+                      ],
+                    ),
+
+                  )
+                ],
+              ),
+            );
+
+
+          // ignore: missing_return
+          }else {
+            return Container();
+          }
+
+
+      },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.green.shade300,
+        currentIndex: _index,
+
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white.withOpacity(.60),
+        selectedFontSize: 14,
+        unselectedFontSize: 14,
+        onTap: (int index) {
+          setState(() {
+            _index =index ;
+          });
+             if(_index == 2){
+               signOut();
+             }
+          // Respond to item press.
+        },
+        items: [
+
+          BottomNavigationBarItem(
+            title: Text('Timer'),
+            icon: Icon(Icons.access_time),
+          ),
+          BottomNavigationBarItem(
+            title: Text('Settings'),
+            icon: Icon(Icons.settings),
+          ),
+          BottomNavigationBarItem(
+            title: Text('LogOut'),
+            icon: Icon(Icons.logout),
+          ),
+
         ],
-      ),);
-   }
-   Future<void> onWrite() async {}
+      )
+
+
+    );
+  }
+
+  //Future<void> onWrite() async {
+   // dbRef.child("Data").set(
+   //     {"Humidity":0, "Temperature":0}
+  //  );
+  //  dbRef.child("UVLightstate").set({"switch": !value}); // write data to database to put lights on with relays
+
+int _index=0;
+
+
+
+
+  //}
+  Future<void> writeData(String nameOfLight, bool valueOfLight) async {
+    dbRef.child(nameOfLight).set({
+      "switch":!valueOfLight
+    });
+  }
+
+
 }
-
-
---
+++ lib/common_widgets/custom_card.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+ class CustomCard  extends StatefulWidget {
+
+
+   Widget build(BuildContext context) {
+     return Container();
+   }
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+ }
+
+
